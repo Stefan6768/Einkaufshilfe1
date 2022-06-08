94b645a87832d7e25b1995064c3067cb76161b68
Stefan <103416259+Stefan6768@users.noreply.github.com>
2022-06-08 09:07:55 +0200

    Überarbeitet

--- src/components/GruppenTag.js
+++ src/components/GruppenTag.js
@@ -12,78 +12,80 @@ import ArtikelTag from './ArtikelTag'
  * @property {Gruppe} gruppe - die darzustellende Gruppe
  */
 class GruppenTag extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      aufgeklappt: true
-    }
-  }
-
-  componentDidMount() {
-    let aufgeklappt = localStorage.getItem("gruppe-" + this.props.gruppe.id)
-    aufgeklappt = (aufgeklappt == null) ? true : JSON.parse(aufgeklappt)
-    this.setState({aufgeklappt: aufgeklappt})
-  }
-  /**
-   *  Entfernt den Artikel
-   * @param name - entfernt name des Artikels
-   */
-  artikelEntfernen(name) {
-    this.props.gruppe.artikelEntfernen(name)
-    this.props.aktiveGruppeHandler(this.props.gruppe)
-  }
-  /**
-   * klappt auf oder zu
-   */
-  aufZuKlappen() {
-    const neuerZustand = !this.state.aufgeklappt
-    localStorage.setItem("gruppe-" + this.props.gruppe.id, neuerZustand)
-    this.setState({aufgeklappt: !this.state.aufgeklappt})
-  }
-
-  render() {
-    const gruppe = this.props.gruppe
-
-    let gruppenHeader = ""
-    if (this.props.gekauft == false) {
-      gruppenHeader = (
-          <dt className={this.props.aktiv ? "aktiv" : "inaktiv"}
-              onClick={() => this.props.aktiveGruppeHandler(gruppe)}>
-            <span>{gruppe.name}</span>
-            <i className="material-icons"
-               onClick={() => this.aufZuKlappen()}>
-              {this.state.aufgeklappt ? 'expand_more' : 'expand_less'}
-            </i>
-          </dt>)
-    }
-
-    let artikelArray = []
-    if (this.state.aufgeklappt) {
-      for (const artikel of gruppe.artikelListe) {
-        if (artikel.gekauft == this.props.gekauft) {
-          artikelArray.push(
-              <ArtikelTag artikel={artikel} key={artikel.id}
-                          checkHandler={this.props.checkHandler}
-                          deleteHandler={() => this.artikelEntfernen(artikel.name)}/>)
+    constructor(props) {
+        super(props)
+        this.state = {
+            aufgeklappt: true
         }
-      }
     }
 
-    return (
-        <React.Fragment>
-          {gruppenHeader}
-          {artikelArray}
-        </React.Fragment>
-    )
-  }
+    componentDidMount() {
+        let aufgeklappt = localStorage.getItem("gruppe-" + this.props.gruppe.id)
+        aufgeklappt = (aufgeklappt == null) ? true : JSON.parse(aufgeklappt)
+        this.setState({aufgeklappt: aufgeklappt})
+    }
+
+    /**
+     *  Entfernt den Artikel
+     * @param name - entfernt name des Artikels
+     */
+    artikelEntfernen(name) {
+        this.props.gruppe.artikelEntfernen(name)
+        this.props.aktiveGruppeHandler(this.props.gruppe)
+    }
+
+    /**
+     * klappt auf oder zu
+     */
+    aufZuKlappen() {
+        const neuerZustand = !this.state.aufgeklappt
+        localStorage.setItem("gruppe-" + this.props.gruppe.id, neuerZustand)
+        this.setState({aufgeklappt: !this.state.aufgeklappt})
+    }
+
+    render() {
+        const gruppe = this.props.gruppe
+
+        let gruppenHeader = ""
+        if (this.props.gekauft == false) {
+            gruppenHeader = (
+                <dt className={this.props.aktiv ? "aktiv" : "inaktiv"}
+                    onClick={() => this.props.aktiveGruppeHandler(gruppe)}>
+                    <span>{gruppe.name}</span>
+                    <i className="material-icons"
+                       onClick={() => this.aufZuKlappen()}>
+                        {this.state.aufgeklappt ? 'expand_more' : 'expand_less'}
+                    </i>
+                </dt>)
+        }
+
+        let artikelArray = []
+        if (this.state.aufgeklappt) {
+            for (const artikel of gruppe.artikelListe) {
+                if (artikel.gekauft == this.props.gekauft) {
+                    artikelArray.push(
+                        <ArtikelTag artikel={artikel} key={artikel.id}
+                                    checkHandler={this.props.checkHandler}
+                                    deleteHandler={() => this.artikelEntfernen(artikel.name)}/>)
+                }
+            }
+        }
+
+        return (
+            <React.Fragment>
+                {gruppenHeader}
+                {artikelArray}
+            </React.Fragment>
+        )
+    }
 }
 
 GruppenTag.propTypes = {
-  aktiv: PropTypes.bool,
-  aktiveGruppeHandler: PropTypes.func.isRequired,
-  checkHandler: PropTypes.func.isRequired,
-  gekauft: PropTypes.bool.isRequired,
-  gruppe: PropTypes.object.isRequired,
+    aktiv: PropTypes.bool,
+    aktiveGruppeHandler: PropTypes.func.isRequired,
+    checkHandler: PropTypes.func.isRequired,
+    gekauft: PropTypes.bool.isRequired,
+    gruppe: PropTypes.object.isRequired,
 
 }
 export default GruppenTag
